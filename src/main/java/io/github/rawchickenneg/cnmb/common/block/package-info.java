610d46e09f5ac4f01e15ad85cb687f581b713de0
glyceryl <494192175@qq.com>
2022-12-09 11:24:03 +0800

    优化部分代码

---
+++ src/main/java/io/github/rawchickenneg/cnmb/common/block/package-info.java
@@ -0,0 +1,9 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+@FieldsAreNonnullByDefault
+package io.github.rawchickenneg.cnmb.common.block;
+
+import net.minecraft.FieldsAreNonnullByDefault;
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
