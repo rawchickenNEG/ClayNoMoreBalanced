36261e139cf61de32ac8995d1413a7b68eba1c55
rawchickenNEG <1589900935@qq.com>
2022-12-11 11:38:51 -0500

    为可投掷铁锭增加修复铁傀儡的功能

--- src/main/java/io/github/rawchickenneg/cnmb/common/entity/ThrownIronIngot.java
+++ src/main/java/io/github/rawchickenneg/cnmb/common/entity/ThrownIronIngot.java
@@ -6,12 +6,15 @@ import io.github.rawchickenneg.cnmb.config.Config;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.IronGolem;
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraftforge.network.NetworkHooks;
@@ -37,14 +40,28 @@ public class ThrownIronIngot extends ThrowableItemProjectile {
 
     protected void onHitEntity(EntityHitResult p_37486_) {
         super.onHitEntity(p_37486_);
-        p_37486_.getEntity().hurt(DamageSource.thrown(this, this.getOwner()), Config.CONFIG.IRON.get());
+        Entity entity = p_37486_.getEntity();
+        if (entity instanceof IronGolem){
+            float f1 = 1.0F + (this.random.nextFloat() - this.random.nextFloat()) * 0.2F;
+            ((IronGolem) entity).heal(25.0F);
+            this.playSound(SoundEvents.IRON_GOLEM_REPAIR, 1.0F, f1);
+        } else {
+            this.spawnAtLocation(ItemRegistry.THROWABLE_IRON_INGOT.get());
+            entity.hurt(DamageSource.thrown(this, this.getOwner()), Config.CONFIG.IRON.get());
+        }
+    }
+
+    protected void onHitBlock(BlockHitResult p_37384_) {
+        super.onHitBlock(p_37384_);
+        if (!this.level.isClientSide) {
+            this.spawnAtLocation(ItemRegistry.THROWABLE_IRON_INGOT.get());
+        }
     }
 
     protected void onHit(HitResult p_37488_) {
         super.onHit(p_37488_);
         if (!this.level.isClientSide) {
             this.playSound(SoundEvents.METAL_PLACE, 1.0F, 1.2F / (this.random.nextFloat() * 0.2F + 0.9F));
-            this.spawnAtLocation(ItemRegistry.THROWABLE_IRON_INGOT.get());
             this.discard();
         }
     }
