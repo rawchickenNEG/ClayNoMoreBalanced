2868efffec4c25f14a011aa66506d46c2c96b6d8
rawchickenNEG <1589900935@qq.com>
2022-12-14 17:09:02 -0500

    修改黏土弓拉弓速度动画

--- src/main/java/io/github/rawchickenneg/cnmb/common/registry/PropertyRegistry.java
+++ src/main/java/io/github/rawchickenneg/cnmb/common/registry/PropertyRegistry.java
@@ -15,7 +15,7 @@ public class PropertyRegistry {
             if (p_174637_ == null) {
                 return 0.0F;
             } else {
-                return p_174637_.getUseItem() != p_174635_ ? 0.0F : (float) (p_174635_.getUseDuration() - p_174637_.getUseItemRemainingTicks()) / 20.0F;
+                return p_174637_.getUseItem() != p_174635_ ? 0.0F : (float) (p_174635_.getUseDuration() - p_174637_.getUseItemRemainingTicks()) / 10.0F;
             }
         });
         ItemProperties.register(ItemRegistry.CLAY_BOW.get(), new ResourceLocation("pulling"), (p_174630_, p_174631_, p_174632_, p_174633_) ->
