bcc34700f5148c5efdcf4f6065efd88fefc9bc1f
rawchickenNEG <1589900935@qq.com>
2022-12-14 17:08:49 -0500

    将黏土弓原有的功能移植至黏土法杖

--- src/main/java/io/github/rawchickenneg/cnmb/common/item/ClayBowItem.java
+++ src/main/java/io/github/rawchickenneg/cnmb/common/item/ClayBowItem.java
@@ -1,6 +1,5 @@
 package io.github.rawchickenneg.cnmb.common.item;
 
-import io.github.rawchickenneg.cnmb.common.entity.ThrownClayBallFromBow;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
@@ -8,7 +7,10 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.*;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 
 import java.util.function.Predicate;
@@ -18,38 +20,75 @@ public class ClayBowItem extends ProjectileWeaponItem implements Vanishable {
         super(p_40660_);
     }
 
-    public void releaseUsing(ItemStack p_40667_, Level p_40668_, LivingEntity p_40669_, int p_40670_) {
-        if (p_40669_ instanceof Player) {
-            Player player = (Player)p_40669_;
-            int i = this.getUseDuration(p_40667_) - p_40670_;
-                float f = getPowerForTime(i);
-                if (!((double)f < 0.1D)) {
-                    if (!p_40668_.isClientSide) {
-                        int count = 1;
-                        if (f > 0.2F) {
-                            count = Math.round(f * 15 + 1);
+    public void releaseUsing(ItemStack pStack, Level pLevel, LivingEntity pEntityLiving, int pTimeLeft) {
+        if (pEntityLiving instanceof Player) {
+            Player player = (Player)pEntityLiving;
+            boolean flag = player.getAbilities().instabuild || EnchantmentHelper.getItemEnchantmentLevel(Enchantments.INFINITY_ARROWS, pStack) > 0;
+            ItemStack itemstack = player.getProjectile(pStack);
 
-                            for (int j = 0; j < count; ++j) {
-                                ThrownClayBallFromBow clayball = new ThrownClayBallFromBow(p_40668_, player);
-                                clayball.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, f * 4.0F, 10.0F);
-                                p_40668_.addFreshEntity(clayball);
-                            }
-                        }
+            int i = this.getUseDuration(pStack) - pTimeLeft;
+            i = net.minecraftforge.event.ForgeEventFactory.onArrowLoose(pStack, pLevel, player, i, !itemstack.isEmpty() || flag);
+            if (i < 0) return;
 
-                        p_40667_.hurtAndBreak(1, player, (p_40665_) -> {
-                            p_40665_.broadcastBreakEvent(player.getUsedItemHand());
-                        });
-                    }
-
-                    p_40668_.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.SNOWBALL_THROW, SoundSource.PLAYERS, 1.0F, 1.0F / (p_40668_.getRandom().nextFloat() * 0.4F + 1.2F) + f * 0.5F);
-                    player.awardStat(Stats.ITEM_USED.get(this));
+            if (!itemstack.isEmpty() || flag) {
+                if (itemstack.isEmpty()) {
+                    itemstack = new ItemStack(Items.ARROW);
                 }
 
+                float f = getPowerForTime(i);
+                if (!((double)f < 0.1D)) {
+                    boolean flag1 = player.getAbilities().instabuild || (itemstack.getItem() instanceof ArrowItem && ((ArrowItem)itemstack.getItem()).isInfinite(itemstack, pStack, player));
+                    if (!pLevel.isClientSide) {
+                        ArrowItem arrowitem = (ArrowItem)(itemstack.getItem() instanceof ArrowItem ? itemstack.getItem() : Items.ARROW);
+                        AbstractArrow abstractarrow = arrowitem.createArrow(pLevel, itemstack, player);
+                        abstractarrow = customArrow(abstractarrow);
+                        abstractarrow.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, f * 6F, 0F);
+                        abstractarrow.setNoGravity(true);
+                        abstractarrow.setBaseDamage(6.0D);
+                        if (f == 1.0F) {
+                            abstractarrow.setCritArrow(true);
+                        }
+
+                        int j = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.POWER_ARROWS, pStack);
+                        if (j > 0) {
+                            abstractarrow.setBaseDamage(abstractarrow.getBaseDamage() + (double)j * 0.5D + 0.5D);
+                        }
+
+                        int k = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.PUNCH_ARROWS, pStack);
+                        if (k > 0) {
+                            abstractarrow.setKnockback(k);
+                        }
+
+                        if (EnchantmentHelper.getItemEnchantmentLevel(Enchantments.FLAMING_ARROWS, pStack) > 0) {
+                            abstractarrow.setSecondsOnFire(100);
+                        }
+
+                        pStack.hurtAndBreak(1, player, (p_40665_) -> {
+                            p_40665_.broadcastBreakEvent(player.getUsedItemHand());
+                        });
+                        if (flag1 || player.getAbilities().instabuild && (itemstack.is(Items.SPECTRAL_ARROW) || itemstack.is(Items.TIPPED_ARROW))) {
+                            abstractarrow.pickup = AbstractArrow.Pickup.CREATIVE_ONLY;
+                        }
+
+                        pLevel.addFreshEntity(abstractarrow);
+                    }
+
+                    pLevel.playSound((Player)null, player.getX(), player.getY(), player.getZ(), SoundEvents.ARROW_SHOOT, SoundSource.PLAYERS, 1.0F, 1.0F / (pLevel.getRandom().nextFloat() * 0.4F + 1.2F) + f * 0.5F);
+                    if (!flag1 && !player.getAbilities().instabuild) {
+                        itemstack.shrink(1);
+                        if (itemstack.isEmpty()) {
+                            player.getInventory().removeItem(itemstack);
+                        }
+                    }
+
+                    player.awardStat(Stats.ITEM_USED.get(this));
+                }
+            }
         }
     }
 
-    public static float getPowerForTime(int p_40662_) {
-        float f = (float)p_40662_ / 20.0F;
+    public static float getPowerForTime(int pCharge) {
+        float f = (float)pCharge / 10.0F;
         f = (f * f + f * 2.0F) / 3.0F;
         if (f > 1.0F) {
             f = 1.0F;
@@ -58,25 +97,36 @@ public class ClayBowItem extends ProjectileWeaponItem implements Vanishable {
         return f;
     }
 
-    public int getUseDuration(ItemStack p_40680_) {
+    public int getUseDuration(ItemStack pStack) {
         return 72000;
     }
 
-    public UseAnim getUseAnimation(ItemStack p_40678_) {
+    public UseAnim getUseAnimation(ItemStack pStack) {
         return UseAnim.BOW;
     }
 
-    public InteractionResultHolder<ItemStack> use(Level p_40672_, Player p_40673_, InteractionHand p_40674_) {
-        ItemStack itemstack = p_40673_.getItemInHand(p_40674_);
-        p_40673_.startUsingItem(p_40674_);
-        return InteractionResultHolder.consume(itemstack);
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
+        ItemStack itemstack = pPlayer.getItemInHand(pHand);
+        boolean flag = !pPlayer.getProjectile(itemstack).isEmpty();
 
+        InteractionResultHolder<ItemStack> ret = net.minecraftforge.event.ForgeEventFactory.onArrowNock(itemstack, pLevel, pPlayer, pHand, flag);
+        if (ret != null) return ret;
+
+        if (!pPlayer.getAbilities().instabuild && !flag) {
+            return InteractionResultHolder.fail(itemstack);
+        } else {
+            pPlayer.startUsingItem(pHand);
+            return InteractionResultHolder.consume(itemstack);
+        }
     }
 
     public Predicate<ItemStack> getAllSupportedProjectiles() {
         return ARROW_ONLY;
     }
 
+    public AbstractArrow customArrow(AbstractArrow arrow) {
+        return arrow;
+    }
 
     public int getDefaultProjectileRange() {
         return 15;
