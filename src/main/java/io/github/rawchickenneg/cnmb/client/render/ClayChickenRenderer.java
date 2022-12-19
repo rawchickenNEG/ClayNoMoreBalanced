34cb6134648c9663a8ca97d5bbf54b599633a25c
glyceryl <494192175@qq.com>
2022-12-19 11:15:53 +0800

    修复黏土鸡无法召唤的问题

--- src/main/java/io/github/rawchickenneg/cnmb/client/render/ClayChickenRenderer.java
+++ src/main/java/io/github/rawchickenneg/cnmb/client/render/ClayChickenRenderer.java
@@ -1,16 +1,32 @@
 package io.github.rawchickenneg.cnmb.client.render;
 
+import io.github.rawchickenneg.cnmb.common.entity.ClayChicken;
+import net.minecraft.client.model.ChickenModel;
+import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.entity.ChickenRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.animal.Chicken;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class ClayChickenRenderer extends ChickenRenderer {
-    public ClayChickenRenderer(EntityRendererProvider.Context p_173952_) {
-        super(p_173952_);
+@OnlyIn(Dist.CLIENT)
+public class ClayChickenRenderer extends MobRenderer<ClayChicken, ChickenModel<ClayChicken>> {
+
+    public ClayChickenRenderer(EntityRendererProvider.Context context) {
+        super(context, new ChickenModel<>(context.bakeLayer(ModelLayers.CHICKEN)), 0.3F);
     }
 
-    public ResourceLocation getTextureLocation(Chicken pEntity) {
+    public ResourceLocation getTextureLocation(ClayChicken entity) {
         return new ResourceLocation("clay_no_more_balanced:textures/entity/clay_chicken.png");
     }
+
+    protected float getBob(ClayChicken chicken, float partialTicks) {
+        float f = Mth.lerp(partialTicks, chicken.oFlap, chicken.flap);
+        float f1 = Mth.lerp(partialTicks, chicken.oFlapSpeed, chicken.flapSpeed);
+        return (Mth.sin(f) + 1.0F) * f1;
+    }
+
 }
--- src/main/java/io/github/rawchickenneg/cnmb/common/registry/ItemRegistry.java
+++ src/main/java/io/github/rawchickenneg/cnmb/common/registry/ItemRegistry.java
@@ -67,7 +67,7 @@ public class ItemRegistry {
     public static final RegistryObject<Item> ENDER_CLAY_BALL = ITEMS.register("ender_clay_ball", () -> new EnderClayBall(defaultBuilder()));
     public static final RegistryObject<Item> EXCHANGE_CLAY_BALL = ITEMS.register("exchange_clay_ball", () -> new ExchangeClayBall(defaultBuilder()));
     public static final RegistryObject<Item> BURN_CLAY_BALL = ITEMS.register("burn_clay_ball", () -> new BurnClayBallItem(defaultBuilder()));
-    public static final RegistryObject<Item> CACTUS_CLAY_BALL = ITEMS.register("cactus_clay_ball", () -> new CactusClayBallItem(defaultBuilder()));
+    //public static final RegistryObject<Item> CACTUS_CLAY_BALL = ITEMS.register("cactus_clay_ball", () -> new CactusClayBallItem(defaultBuilder()));
     public static final RegistryObject<Item> TNT_CLAY_BALL = ITEMS.register("tnt_clay_ball", () -> new TNTClayBall(defaultBuilder()));
     public static final RegistryObject<Item> EXPLODE_CLAY_BALL = ITEMS.register("explode_clay_ball", () -> new ExplodeClayBallItem(defaultBuilder()));
     public static final RegistryObject<Item> ADVANCED_EXPLODE_CLAY_BALL = ITEMS.register("advanced_explode_clay_ball", () -> new AdvancedExplodeClayBallItem(defaultBuilder()));
