17c9bc7ba3074286bed4ef021dbd337cd901f827
rawchickenNEG <1589900935@qq.com>
2022-12-18 21:43:41 -0500

    可投掷箭现在会根据力量等级决定投掷出的速度

--- src/main/java/io/github/rawchickenneg/cnmb/common/item/throwableitems/ThrowableArrowItem.java
+++ src/main/java/io/github/rawchickenneg/cnmb/common/item/throwableitems/ThrowableArrowItem.java
@@ -8,6 +8,7 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Arrow;
 import net.minecraft.world.item.Item;
@@ -31,8 +32,14 @@ public class ThrowableArrowItem extends Item {
         level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.SNOWBALL_THROW, SoundSource.NEUTRAL, 0.5F, 0.4F / (level.random.nextFloat() * 0.4F + 0.8F));
         if (!level.isClientSide) {
             Arrow arrow = new Arrow(level, player);
-            arrow.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 1.5F, 1.0F);
-            level.addFreshEntity(arrow);
+            if (player.hasEffect(MobEffects.DAMAGE_BOOST)){
+                float strength = (player.getEffect(MobEffects.DAMAGE_BOOST).getAmplifier());
+                arrow.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 2.5F + strength, 1.0F);
+                level.addFreshEntity(arrow);
+            } else {
+                arrow.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 1.5F, 1.0F);
+                level.addFreshEntity(arrow);
+            }
         }
 
         player.awardStat(Stats.ITEM_USED.get(this));
