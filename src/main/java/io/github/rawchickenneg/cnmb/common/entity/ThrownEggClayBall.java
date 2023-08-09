ad5dc6abb9e3cef6b551695d2e63e23b8faec7fa
rawchickenNEG <1589900935@qq.com>
2023-08-09 00:11:21 -0400

    修改刷怪蛋获取逻辑

--- src/main/java/io/github/rawchickenneg/cnmb/common/entity/ThrownEggClayBall.java
+++ src/main/java/io/github/rawchickenneg/cnmb/common/entity/ThrownEggClayBall.java
@@ -36,28 +36,46 @@ public class ThrownEggClayBall extends ThrowableItemProjectile {
         return ItemRegistry.EGG_CLAY_BALL.get();
     }
 
+    //public void tick() {
+    //    super.tick();
+    //    final Vec3 center = new Vec3(this.getX(), this.getY(), this.getZ());
+    //    List<Entity> entities = this.getLevel().getEntitiesOfClass(Entity.class, new AABB(center, center).inflate(5), e -> true).stream()
+    //            .sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(center))).toList();
+    //    for (Entity target : entities) {
+    //        if (target instanceof EnderDragon enderDragon && enderDragon.getHealth() <= 10.0F){
+    //            this.spawnAtLocation(Items.DRAGON_EGG);
+    //            enderDragon.hurt(DamageSource.playerAttack((Player) Objects.requireNonNull(this.getOwner())),200);
+    //            this.discard();
+    //        }
+    //    }
+    //}
+
     protected void onHitEntity(EntityHitResult p_37486_) {
         super.onHitEntity(p_37486_);
         if (p_37486_.getEntity() instanceof LivingEntity livingEntity) {
             ItemStack egg = new ItemStack(ForgeSpawnEggItem.fromEntityType(livingEntity.getType()));
-            if (egg.getItem() != ItemStack.EMPTY.getItem() && livingEntity.getHealth() <= 10.0F){
-                this.spawnAtLocation(egg);
+            if (livingEntity.getHealth() <= 10.0F){
+                if (egg.getItem() != ItemStack.EMPTY.getItem()){
+                    this.spawnAtLocation(egg);
+                }
                 Level pLevel = this.getLevel();
                 pLevel.levelEvent(2009, livingEntity.getOnPos(), 0);
                 this.playSound(SoundEvents.PUFFER_FISH_BLOW_UP , 1.0F, 1.2F / (this.random.nextFloat() * 0.2F + 0.9F));
                 livingEntity.discard();
                 this.discard();
-            } else {
-                p_37486_.getEntity().hurt(DamageSource.thrown(this, this.getOwner()), Config.CONFIG.CLAY.get());
-                this.spawnAtLocation(ItemRegistry.EGG_CLAY_BALL.get());
-                this.discard();
             }
         }
+        p_37486_.getEntity().hurt(DamageSource.thrown(this, this.getOwner()), Config.CONFIG.EGG.get());
+        this.playSound(SoundEvents.TURTLE_EGG_BREAK , 1.0F, 1.2F / (this.random.nextFloat() * 0.2F + 0.9F));
+        this.spawnAtLocation(ItemRegistry.EGG_CLAY_BALL.get());
+        this.discard();
     }
+
 
     protected void onHitBlock(BlockHitResult p_37488_) {
         super.onHitBlock(p_37488_);
         if (!this.level.isClientSide) {
+            this.playSound(SoundEvents.TURTLE_EGG_BREAK , 1.0F, 1.2F / (this.random.nextFloat() * 0.2F + 0.9F));
             this.spawnAtLocation(ItemRegistry.EGG_CLAY_BALL.get());
             this.discard();
         }
